66e2cff8df5b12c5617540ac1d1dce8949a94d95
neu-astha <neuastha12@gmail.com>
2023-10-04 22:16:45 +0545

    incomplete tasks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var swiper = new Swiper(".homeSwiper", {
+    loop: true,
+    effect: "fade",
+    speed: 2000,
+    fadeEffect: {
+        crossFade: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
       dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".fa-solid",
     },
   });
